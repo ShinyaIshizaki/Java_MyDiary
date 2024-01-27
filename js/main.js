9c7bf7fbbcf59ae7288b006b65e3df5f28647587
Shinya Ishizaki <shinya.ishizaki.0311@gmail.com>
2024-01-27 23:16:46 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function callJavaApp() {
+  // Javaプログラムを呼び出す
+  console.log("***Start function callApp***");
+
+  console.log("***End function callApp***");
+}
+
+document.getElementById('callApp').onclick = callJavaApp;
